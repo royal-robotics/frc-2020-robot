9d551b5dc70401a2dfa7ce60ddfa533967263215
Larissa Meyer <meyerlar000@edmonds15.org>
2020-01-20 14:13:48 -0800

    test encoder code (might not work)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,73 +6,104 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import edu.wpi.first.wpilibj.*;
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMaxLowLevel.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
   private Joystick _controller;
+  private static final int kCanID = 1;
+  private static final MotorType kMotorType = MotorType.kBrushless;
+  private static final AlternateEncoderType kAltEncType = AlternateEncoderType.kQuadrature;
+  private static final int kCPR = 8192;
   
-  private WPI_TalonSRX _rightDrive1 = new WPI_TalonSRX(8);
-  private WPI_VictorSPX _rightDrive2 = new WPI_VictorSPX(6);
-  private WPI_VictorSPX _rightDrive3 = new WPI_VictorSPX(4);
-
-  private WPI_TalonSRX _leftDrive1 = new WPI_TalonSRX(7);
-  private WPI_VictorSPX _leftDrive2 = new WPI_VictorSPX(5);
-  private WPI_VictorSPX _leftDrive3 = new WPI_VictorSPX(3);
+  private CANSparkMax m_motor;
+  private CANPIDController m_pidController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
         
+  private CANEncoder m_alternateEncoder;
 
   @Override
   public void robotInit() {
-    //this is a comment
+    m_motor = new CANSparkMax(kCanID, kMotorType);
+    m_alternateEncoder = m_motor.getAlternateEncoder(kAltEncType, kCPR);
+    m_pidController = m_motor.getPIDController();
+    m_pidController.setFeedbackDevice(m_alternateEncoder);
+
+    kP = 0.1; 
+    kI = 1e-4;
+    kD = 1; 
+    kIz = 0; 
+    kFF = 0; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
+  
+
     _controller = new Joystick(0);
-
-    _leftDrive2.follow(_leftDrive1);
-    _leftDrive3.follow(_leftDrive1);
-
-    _rightDrive2.follow(_rightDrive1);
-    _rightDrive3.follow(_rightDrive1);
   }
 
   @Override
   public void teleopPeriodic() {
     //boolean a_pressed= false;
     float speed_modifier = 0.1f;
-    /*
-    if (_controller.getRawButton(1))
-    {
-      _leftDrive1.set(-speed_modifier*_controller.getRawAxis(1));
-      _rightDrive1.set(speed_modifier*_controller.getRawAxis(5));
-    }
-    else 
-    {
-      _leftDrive1.set(-_controller.getRawAxis(1));
-      _rightDrive1.set(_controller.getRawAxis(5));
-    }
-    */
-    
-    //System.out.println(_controller.getRawAxis(0));
-    Boolean toggle;
-    //false means normal speed
-    //true means speed set to 0.1
-    if (_controller.getRawButton(1))
-    {
-     toggle = false;
+   
+     double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
     }
 
-    else if (_controller.getRawButton(2))
-    {
-      toggle = true;
-    }
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.ControlType.kDutyCycle
+     *  com.revrobotics.ControlType.kPosition
+     *  com.revrobotics.ControlType.kVelocity
+     *  com.revrobotics.ControlType.kVoltage
+     */
+    m_pidController.setReference(rotations, ControlType.kPosition);
+    
+    SmartDashboard.putNumber("SetPoint", rotations);
+    SmartDashboard.putNumber("ProcessVariable", m_alternateEncoder.getPosition());
+
+   
   }
 
 }
