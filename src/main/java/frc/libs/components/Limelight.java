bdc8333522e801ebaf52252a9ca2e3fe842e6414
Programmer <fakeemail@fakeemail.com>
2020-02-10 20:47:56 -0800

    Added limelight class

---
+++ src/main/java/frc/libs/components/Limelight.java
@@ -0,0 +1,29 @@
+package frc.libs.components;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.DataTable;
+
+public class Limelight {
+    private NetworkTable _table;
+
+    public Limelight() {
+        _table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public boolean hasTarget() {
+        return _table.getEntry("tv").getNumber(0.0).equals(1.0);
+    }
+
+    public double xTarget() {
+        return _table.getEntry("tx").getNumber(0.0).doubleValue();
+    }
+
+    public double yTarget() {
+        return _table.getEntry("ty").getNumber(0.0).doubleValue();
+    }
+
+    public void setPipeline(int pipeline) {
+        _table.getEntry("pipeline").setNumber(pipeline);
+    }
+}
