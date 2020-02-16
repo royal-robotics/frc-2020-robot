39ea0886a75593744d88a6459823fe56697e6de0
filiahlab <fakeemail@fakeemail.com>
2020-02-16 15:58:13 -0800

    Added PID control to climber subsystem

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -13,6 +13,8 @@ public class ClimberSubsystem extends RoyalSubsystem {
     private final EncoderGroup _elevatorEncoder;
     private final WPI_TalonSRX _balance;
 
+    private final ElevatorPidController _elevatorPID;
+
     public ClimberSubsystem() {
         _elevator = Components.Climber.elevator1;
         final var elevator2 = Components.Climber.elevator2;
@@ -22,13 +24,25 @@ public class ClimberSubsystem extends RoyalSubsystem {
         _elevatorEncoder = new EncoderGroup(inchesPerTurn, false, _elevator.getEncoder(), elevator2.getEncoder());
 
         _balance = Components.Climber.balance;
+
+        _elevatorPID = new ElevatorPidController();
     }
 
-    public void setHeight() {
-
+    public void setHeight(double heightInches) {
+        _elevatorPID.setSetpoint(heightInches);
     }
 
     public double getHeight() {
         return _elevatorEncoder.getPosition();
+    }
+
+    public void reinitializeHeight(double heightInches) {
+        _elevatorPID.reset();
+        _elevatorPID.setSetpoint(heightInches);
+    }
+
+    @Override
+    public void periodic() {
+        _elevator.set(_elevatorPID.calculate(getHeight()));
     }
 }
