4e515de4d0da40db6f5d157327cd8229e1ce7279
filiahlab <fakeemail@fakeemail.com>
2020-02-16 22:06:24 -0800

    Add spin wheel timeout :)

--- src/main/java/frc/robot/commands/SpinWheelNumTimes.java
+++ src/main/java/frc/robot/commands/SpinWheelNumTimes.java
@@ -1,30 +1,44 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.colorwheel.*;
 
 public class SpinWheelNumTimes extends CommandBase {
     private final ColorWheelSubsystem _colorWheel;
     private final int _numSpins;
+    private final Timer _timeoutTimer;
 
     public SpinWheelNumTimes(ColorWheelSubsystem colorWheel, int numSpins) {
         _colorWheel = colorWheel;
         _numSpins = numSpins;
+        _timeoutTimer = new Timer();
     }
 
     @Override
     public void initialize() {
+        _timeoutTimer.reset();
+        _timeoutTimer.start();
         _colorWheel.getEncoder().reset();
         _colorWheel.setPower(1.0);
     }
 
     @Override
     public boolean isFinished() {
+        // Turn the motor off if some idiot hit the button
+        // and we're not at the color wheel.
+        final var TimeoutSeconds = 5.0;
+        if (_timeoutTimer.hasPeriodPassed(TimeoutSeconds)) {
+            System.out.println("Warning: Sping wheel timeout");
+            return true;
+        }
+
         return _colorWheel.getEncoder().getTurns() >= _numSpins;
     }
 
     @Override
     public void end(boolean interrupted) {
+        _timeoutTimer.stop();
         _colorWheel.setPower(0.0);
     }
 }
--- src/main/java/frc/robot/commands/SpinWheelToColor.java
+++ src/main/java/frc/robot/commands/SpinWheelToColor.java
@@ -1,32 +1,46 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.util.*;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.colorwheel.*;
 
 public class SpinWheelToColor extends CommandBase {
     private final ColorWheelSubsystem _colorWheel;
     private final Color _color;
+    private final Timer _timeoutTimer;
 
     public SpinWheelToColor(ColorWheelSubsystem colorWheel, Color color) {
         addRequirements(colorWheel);
         _colorWheel = colorWheel;
         _color = color;
+        _timeoutTimer = new Timer();
     }
 
     @Override
     public void initialize() {
+        _timeoutTimer.reset();
+        _timeoutTimer.start();
         _colorWheel.setPower(0.8);
     }
 
     @Override
     public boolean isFinished() {
+        // Turn the motor off if some idiot hit the button
+        // and we're not at the color wheel.
+        final var TimeoutSeconds = 5.0;
+        if (_timeoutTimer.hasPeriodPassed(TimeoutSeconds)) {
+            System.out.println("Warning: Sping wheel timeout");
+            return true;
+        }
+
         return _colorWheel.getEncoder().getColor().equals(_color);
     }
 
 
     @Override
     public void end(boolean interrupted) {
+        _timeoutTimer.stop();
         _colorWheel.setPower(0.0);
     }
 }
